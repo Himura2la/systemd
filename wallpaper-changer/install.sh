e25e92ad2bea0c22e9f9300459b87e442540cb12
Vladislav Glagolev <glagol15@gmail.com>
2026-01-13 11:14:14 +0000

    debug (still not works)

--- wallpaper-changer/install.sh
+++ wallpaper-changer/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 
+
 base_path=https://raw.githubusercontent.com/Himura2la/systemd/refs/heads/master/wallpaper-changer
 
 sudo wget -O /etc/systemd/system/wallpaper-changer.service $base_path/wallpaper-changer.service
@@ -8,5 +9,6 @@ sudo wget -O /etc/systemd/system/wallpaper-changer.timer   $base_path/wallpaper-
 sudo wget -O /opt/set-random-wallpaper.sh                  $base_path/set-random-wallpaper.sh
 sudo chmod +x /opt/set-random-wallpaper.sh
 
+set +x
 echo "Please, set your user and wallpapers dir in '/etc/systemd/system/wallpaper-changer.service'"
 echo "after that, do 'sudo systemctl daemon-reload && sudo systemctl enable wallpaper-changer.timer'"
